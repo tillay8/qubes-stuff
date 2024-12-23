6bd5c806191ebfe3631d6befdaf35eddb027370a
Tilley <tilleyy8@gmail.com>
2024-12-23 16:00:52 -0700

    Create screenshot.sh

---
+++ screenshot.sh
@@ -0,0 +1,6 @@
+QUBE="personal"
+SCREENSHOT="/home/$USER/screenshot.png"
+INCOMING='/home/user/QubesIncoming/dom0/screenshot.png'
+xfce4-screenshooter -r -s $SCREENSHOT
+qvm-move-to-vm $QUBE $SCREENSHOT
+qvm-run --pass-io $QUBE -- 'xclip -selection clipboard -t image/png -i '$INCOMING' &&rm '$INCOMING''
