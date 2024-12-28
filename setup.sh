00542710e1e8a35fc08dd3b94b1db737600c3c3a
Tilley <tilleyy8@gmail.com>
2024-12-27 19:37:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 cd
 rm -rf ~/.oh-my-zsh
 echo "setting qube $(hostname) to tilley's prefs"
+echo "installing zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir ~/.config/fastfetch
-cd ~/.config/fastfetch
-wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/config.jsonc
-cd ~/.config/xfce4/xfconf/xfce-perchannel-xml
-wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/xfce4-terminal.xml
+echo "please run setup2.sh now! (./setup2.sh)"
 cd
 
