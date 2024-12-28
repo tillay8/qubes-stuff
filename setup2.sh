2b8acdcf77a993d20a1cbac93830c5ac5aecb01f
Tilley <tilleyy8@gmail.com>
2024-12-27 19:37:56 -0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -3,5 +3,10 @@ git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 rm ~/.zshrc
 wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/.zshrc
+mkdir ~/.config/fastfetch
+cd ~/.config/fastfetch
+wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/config.jsonc
+cd ~/.config/xfce4/xfconf/xfce-perchannel-xml
+wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/xfce4-terminal.xml
 echo exec zsh > ~/.bashrc
 exec zsh
