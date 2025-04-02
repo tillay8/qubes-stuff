fb11d13e74a0f3113746ee0d47123ff4c5ef3288
Tilley <tilleyy8@gmail.com>
2025-04-02 10:47:26 -0600

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-cd
-rm -rf ~/.oh-my-zsh
-echo "setting qube $(hostname) to tilley's prefs"
-echo "installing zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "please run setup2.sh now! (./setup2.sh)"
-cd
 
+echo "setting qube $(hostname) to tilley's prefs"
+sudo apt install zsh git
+sudo dnf install zsh git
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/.zshrc
+chsh -s $(which zsh)
+exec zsh
