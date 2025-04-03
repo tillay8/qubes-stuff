8cd7bdec0bdf45b0777020a263ad0e8a1181d385
Tilley <tilleyy8@gmail.com>
2025-04-02 19:34:40 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 wget https://raw.githubusercontent.com/tillay8/qubes-stuff/refs/heads/main/.zshrc
-sudo chsh -s $(which zsh)
 exec zsh
