147642e0c25c66c68920d3d555d5c20ac495bb8e
Tilley <tilleyy8@gmail.com>
2025-04-02 19:35:12 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 echo "setting qube $(hostname) to tilley's prefs"
 sudo apt install zsh git
-sudo dnf install zsh git
+#sudo dnf install zsh git
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
